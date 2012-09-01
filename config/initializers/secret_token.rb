a61e5d5089c0eba2d33f29659292efe3fd14c05b
Ken Mazaika <kenmazaika@gmail.com>
2012-09-01 16:38:50 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmgCi::Application.config.secret_token = '3f825d78093e7f8b16f2a42cdf2b96e6732a486c815122a9227a7632359c64605175b970cbacafe545d1aa0c7d791bd1fc2acef9b7659b9607c8dc452ff30b01'
